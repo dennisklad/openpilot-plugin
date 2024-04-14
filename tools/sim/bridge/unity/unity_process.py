701fcf52facb7ece168105ba29cf3679cbb4b99c
Dennis <dennisklad@gmail.com>
2024-04-14 15:36:56 +0200

    Drop MSS, Socket to Unity Render

--- tools/sim/bridge/unity/unity_process.py
+++ tools/sim/bridge/unity/unity_process.py
@@ -4,6 +4,7 @@ from mss import mss
 from PIL import Image
 import time
 import zmq
+import io
 
 from collections import namedtuple
 from panda3d.core import Vec3
@@ -27,20 +28,26 @@ def unity_process(camera_array, wide_camera_array, image_lock, controls_recv: Co
   MAX_STEERING = 0
   rcv = ""
 
-  def get_image():
-      with mss() as sct:
-        # Problem: MSS only captures monitor and not windows!
-        # PyGetWindow and win32gui currently does not support Linux. (?)
-        # TODO: Main monitor is the second one detected. Add this and the size as argument.
-        main_monitor = sct.monitors[1]
-        
-        # Get raw pixels from the screen
-        monitor = {"top": main_monitor['top']+100, "left": main_monitor['left']+100, "width": W, "height": H}
-        
-        sct_img = sct.grab(monitor)
-        img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
-        return np.array(img).reshape((H, W, 3))
+  def get_image_with_mss():
+    """This was a prototype. It is better if we get the frame immediately from Unity!"""  
+    with mss() as sct:
+      # Problem: MSS only captures monitor and not windows!
+      # PyGetWindow and win32gui currently does not support Linux. (?)
+      # TODO: Main monitor is the second one detected. Add this and the size as argument.
+      main_monitor = sct.monitors[1]
+      # Get raw pixels from the screen
+      monitor = {"top": main_monitor['top']+100, "left": main_monitor['left']+100, "width": W, "height": H}
+      sct_img = sct.grab(monitor)
+      img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
+      return np.array(img).reshape((H, W, 3))
 
+  def get_image():
+
+    ss = screen_socket.recv()
+    image = Image.open(io.BytesIO(ss)).convert('RGB')
+         
+    return image
+  
   def step(vc):
     """Executes a step by pushing the controls to the controls_socket.
     This string is then processed by the unity PULL socket.
@@ -102,6 +109,10 @@ def unity_process(camera_array, wide_camera_array, image_lock, controls_recv: Co
   # TODO: Is this necessary? Does it work better without?
   # controls_socket.setsockopt(zmq.CONFLATE, 1)
   controls_socket.bind("tcp://127.0.0.1:5556")
+  
+  screen_socket = zmq.Context().socket(zmq.PULL)
+  screen_socket.setsockopt(zmq.CONFLATE, 1)
+  screen_socket.bind("tcp://127.0.0.1:5558")
   
   state_recv_thread = Thread(target=get_state)
   state_recv_thread.start()
